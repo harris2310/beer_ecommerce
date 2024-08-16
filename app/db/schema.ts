d1e77cfd8c7f810c6bf8ab517a784f52471e9884
Harris Hanozidis <harris2310@hotmail.com>
2024-08-16 10:30:46 +0300

    feat(db): updated schema with new tables

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,5 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
 
 const userTable = sqliteTable("user", {
   id: text("id").notNull().primaryKey(),
@@ -15,4 +16,40 @@ const sessionTable = sqliteTable("session", {
   expiresAt: integer("expires_at").notNull(),
 });
 
-export { userTable, sessionTable };
+const beerTable = sqliteTable("beer", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  image: text("image"),
+  type: text("type"),
+  inStock: integer("in_stock", { mode: "boolean" }),
+  quantity: integer("quantity"),
+  order: integer("order").references(() => orderTable.id),
+});
+
+const orderTable = sqliteTable("order", {
+  id: text("id").notNull().primaryKey(),
+  customerId: integer("customer_id")
+    .notNull()
+    .references(() => userTable.id),
+  orderDate: text("date").default(sql`(CURRENT_DATE)`),
+  status: text("status"),
+});
+
+const cartTable = sqliteTable("cart", {
+  id: text("id").notNull().primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+});
+
+const cartItemTable = sqliteTable("cartItem", {
+  id: text("id").notNull().primaryKey(),
+  cartId: integer("cart_id")
+    .notNull()
+    .references(() => cartTable.id),
+  beerId: integer("beer_id")
+    .notNull()
+    .references(() => beerTable.id),
+});
+
+export { userTable, sessionTable, cartTable, cartItemTable, orderTable, beerTable };
